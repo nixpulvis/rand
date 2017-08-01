0ec8aefb89e537942805bdc5bd4c43fb0862ba74
Diggory Hardy <git@dhardy.name>
2017-08-01 11:05:45 +0100

    Update doc

--- rand/sequence/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["sample","Randomly sample up to `amount` elements from a finite iterator. The order of elements in the sample is not random."]],"trait":[["Choose","This trait implements a `choose` operations on slices and sequences."],["Shuffle","This trait introduces a `shuffle` operations on slices."]]});
